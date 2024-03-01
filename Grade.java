b7621ff00aefbbb23bbb63faf83fdb6862953811
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-03-01 19:42:01 +0530

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,39 @@
+class Grade
+ {
+   public static void main(String[]args)
+    {
+      int m=78;
+
+      if(m>=90&&m<=100)
+         {
+          System.out.println("A+");
+         }
+      else if(m>=80&&m<90)
+         {
+          System.out.println('A');
+         }
+      else if(m>=70&&m<80)
+         {
+          System.out.println('B');
+         }
+      else if(m>=60&&m<70)
+         {
+          System.out.println('C');
+         }
+      else if(m>=50&&m<60)
+         {
+          System.out.println('D');
+         }
+      else if(m>35&&m<50)
+         { 
+          System.out.println('E');
+         }
+      else 
+         {
+          System.out.println("FAIL");
+         }
+    }
+ }
+
+         
+      
